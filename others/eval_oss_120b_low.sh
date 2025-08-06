3b90b11759538fb73905cec03895e1b7c49fb1e9
y726wang <ubo.wang.sunny@gmail.com>
2025-08-05 22:26:00 -0400

    add

--- others/eval_oss_120b_low.sh
+++ others/eval_oss_120b_low.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 MODEL_PATH="/data/minimax-dialogue/feishan/models/gpt-oss-120b"  # 替换为你的模型路径
-OUTPUT_FILE="outputs_gpt-oss-120b_high.json"        # 输出文件名
-REASONING_EFFORT="high"
+OUTPUT_FILE="outputs_gpt-oss-120b_low.json"        # 输出文件名
+REASONING_EFFORT="low"
 
 python your_script.py \
   --model_path "$MODEL_PATH" \
--- others/eval_oss_120b_medium.sh
+++ others/eval_oss_120b_medium.sh
@@ -3,8 +3,8 @@
 export CUDA_VISIBLE_DEVICES=1
 
 MODEL_PATH="/data/minimax-dialogue/feishan/models/gpt-oss-120b"  # 替换为你的模型路径
-OUTPUT_FILE="outputs_gpt-oss-120b_high.json"        # 输出文件名
-REASONING_EFFORT="high"
+OUTPUT_FILE="outputs_gpt-oss-120b_medium.json"        # 输出文件名
+REASONING_EFFORT="medium"
 
 python your_script.py \
   --model_path "$MODEL_PATH" \
