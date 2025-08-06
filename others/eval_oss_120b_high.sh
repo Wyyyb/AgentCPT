c54b0a6cdf4e88bd0bee4c6d5565ef732a79224d
y726wang <ubo.wang.sunny@gmail.com>
2025-08-05 23:02:59 -0400

    add

--- others/eval_oss_120b_high.sh
+++ others/eval_oss_120b_high.sh
@@ -2,7 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=1
 
-MODEL_PATH="/data/minimax-dialogue/feishan/models/gpt-oss-120b"  # 替换为你的模型路径
+MODEL_PATH="openai/gpt-oss-120b"  # 替换为你的模型路径
 OUTPUT_FILE="outputs_gpt-oss-120b_high.json"        # 输出文件名
 REASONING_EFFORT="high"
 
